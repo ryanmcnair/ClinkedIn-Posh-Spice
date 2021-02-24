8735d56dd0b44d65c1da919af4d06d9f9f3a6d30
Chris LoJacono <clojacono92@gmail.com>
2021-02-23 18:14:32 -0600

    repository done

--- ClinkedIn/DataAccess/MemberRepository.cs
+++ ClinkedIn/DataAccess/MemberRepository.cs
@@ -10,11 +10,39 @@ namespace ClinkedIn.DataAccess
     {
         static List<Member> _allMembers = new List<Member>
             {
-                new Member {InmateId = 1, Name="Prison Mike" }
-               
+                new Member {
+                    InmateId = 1,
+                    Name="Prison Mike",
+                    MemberInterests = new List<Interest>
+                    {
+                       new Interest{ Id = 1, Interests = "Playing Spades", Type= "Cards"},
+                       new Interest{ Id = 2, Interests = "Playing Solitare", Type= "Cards"},
+                       new Interest{ Id = 3, Interests = "Lifting Weights", Type= "Exercise"},
+                    }
+                },
+                new Member {
+                    InmateId = 2,
+                    Name="Big Billy Bruno",
+                    MemberInterests = new List<Interest>
+                    {
+                       new Interest{ Id = 4, Interests = "Slam Poetry", Type= "Art"},
+                       new Interest{ Id = 2, Interests = "Playing Solitare", Type= "Cards"},
+                       new Interest{ Id = 6, Interests = "Doing Push-ups", Type= "Exercise"},
+                    }
+                },
+                new Member {
+                    InmateId = 3,
+                    Name="Ghosteface Killa",
+                    MemberInterests = new List<Interest>
+                    {
+                       new Interest{ Id = 4, Interests = "Slam Poetry", Type= "Art"},
+                       new Interest{ Id = 7, Interests = "Juggling Shanks", Type= "Entertainment"},
+                       new Interest{ Id = 8, Interests = "Community Puppet Shows", Type= "Entertainment"},
+                    }
+                }
             };
 
-        public List<Member> GetAll()
+        public List<Member> GetAllMembers()
         {
             return _allMembers;
         }
@@ -25,15 +53,15 @@ namespace ClinkedIn.DataAccess
             member.InmateId = biggestExistingId + 1;
             _allMembers.Add(member);
         }
-        public Member Get(int id)
+        public Member GetAMember(int id)
         {
             var member = _allMembers.FirstOrDefault(memb => memb.InmateId == id);
             return member;
         }
 
-        public void Remove(int id)
+        public void RemoveAMember(int id)
         {
-            var memberToRemove = Get(id);
+            var memberToRemove = GetAMember(id);
             _allMembers.Remove(memberToRemove);
         }
     }
