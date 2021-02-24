55e1f6081cd2e7be527ccc77aefd6843d69a773c
Chris LoJacono <clojacono92@gmail.com>
2021-02-23 19:09:40 -0600

    done with member model, repo and controller

--- ClinkedIn/Models/Member.cs
+++ ClinkedIn/Models/Member.cs
@@ -7,10 +7,12 @@ namespace ClinkedIn.Models
 {
     public class Member
     {
+
+        public int InmateId { get; set; }
         public string Name { get; set; }
         public List<Interest> MemberInterests { get; set; } = new List<Interest>();
         public List<Service> MemberServices { get; set; } = new List<Service>();
-        public int InmateId { get; set; }
+
     }
 }
 
