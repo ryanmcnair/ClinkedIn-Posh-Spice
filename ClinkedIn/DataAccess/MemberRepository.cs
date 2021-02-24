027cb1cbc770168fbb9689e0efa9204ffccd10ac
josephtmartin <goldglovejo13@gmail.com>
2021-02-23 20:25:41 -0600

    merged main and fixed conflicts

--- ClinkedIn/DataAccess/MemberRepository.cs
+++ ClinkedIn/DataAccess/MemberRepository.cs
@@ -24,8 +24,7 @@ namespace ClinkedIn.DataAccess
                         new Service{ Id = 1, Services = "Cleaning blood"},
                         new Service{ Id = 2, Services = "Creating blood"},
                         new Service{ Id = 3, Services = "Starting Fight"}
-                    }
-                   
+                    },
                 },
                 new Member {
                     InmateId = 2,
@@ -34,20 +33,31 @@ namespace ClinkedIn.DataAccess
                     {
                        new Interest{ Id = 4, Interests = "Slam Poetry", Type= InterestType.Arts},
                        new Interest{ Id = 2, Interests = "Playing Solitare", Type= InterestType.Gaming},
-                       new Interest{ Id = 6, Interests = "Doing Push-ups", Type= InterestType.Exercise,
+                       new Interest{ Id = 6, Interests = "Doing Push-ups", Type= InterestType.Exercise},
                     },
-                },
+                    MemberServices = new List<Service>
+                    {
+                        new Service{ Id = 1, Services = "Shanking a Guard"},
+                        new Service{ Id = 2, Services = "Personal Trainer"},
+                        new Service{ Id = 3, Services = "Manufacturing Weapons"}
+                    }
                 },
                 new Member {
                     InmateId = 3,
                     Name="Ghosteface Killa",
                     MemberInterests = new List<Interest>
                     {
-                       new Interest{ Id = 4, Interests = "Slam Poetry", Type= "Art"},
-                       new Interest{ Id = 7, Interests = "Juggling Shanks", Type= "Entertainment"},
-                       new Interest{ Id = 8, Interests = "Community Puppet Shows", Type= "Entertainment"},
+                       new Interest{ Id = 4, Interests = "Slam Poetry", Type= InterestType.Arts},
+                       new Interest{ Id = 7, Interests = "Juggling Shanks", Type= InterestType.Arts},
+                       new Interest{ Id = 8, Interests = "Community Puppet Shows", Type= InterestType.Arts},
+                    },
+                    MemberServices = new List<Service>
+                    {
+                        new Service{ Id = 1, Services = "Laundry"},
+                        new Service{ Id = 2, Services = "Back Massage"},
+                        new Service{ Id = 3, Services = "Shower Assistant"}
                     }
-                }
+                },
             };
         
         public List<Member> GetAllMembers()
