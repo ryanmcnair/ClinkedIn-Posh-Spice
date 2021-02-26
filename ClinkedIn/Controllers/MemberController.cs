99b254ad26db3a2aa56fcca50f786620709d70d8
josephtmartin <goldglovejo13@gmail.com>
2021-02-25 19:42:15 -0600

    working on adding the friends functionality

--- ClinkedIn/Controllers/MemberController.cs
+++ ClinkedIn/Controllers/MemberController.cs
@@ -37,8 +37,28 @@ namespace ClinkedIn.Controllers
         [HttpGet("{inmateId}")]
         public IActionResult GetASingleMember(int inmateId)
         {
-
             return Ok(_memberRepo.GetAMember(inmateId));
+        }
+
+        //Add A Friend
+        [HttpPut("{id}/add-friend-{friendId}")]
+        public IActionResult AddFriend(int id, int friendId)
+        {
+            var clinker = _memberRepo.GetAMember(id);
+            var friend = _memberRepo.GetAMember(friendId);
+            clinker.Friends.Add(friend);
+            friend.Friends.Add(clinker);
+            return Ok($"You have added {friend.Name} as a friend");
+        }
+
+        //Get friends of a member
+        [HttpGet("{id}/friends")]
+        public IActionResult GetFriends(int id)
+        {
+            var clinker = _memberRepo.GetAMember(id);
+            if (clinker == null) return NotFound($"There is no clinker with an id of: {id}");
+            if (clinker.Friends.Count == 0) return NotFound($"{clinker.Name} has no friends... What a loser!");
+            return Ok(clinker.Friends);
         }
     }
 }
--- ClinkedIn/Models/Member.cs
+++ ClinkedIn/Models/Member.cs
@@ -12,7 +12,7 @@ namespace ClinkedIn.Models
         public string Name { get; set; }
         public List<Interest> MemberInterests { get; set; } = new List<Interest>();
         public List<Service> MemberServices { get; set; } = new List<Service>();
-
+        public List<Member> Friends { get; set; } = new List<Member>();
     }
 }
 
